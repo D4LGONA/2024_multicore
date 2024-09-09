ba35e55ccc379546c7adc99e3a31f60eef27d779
D4LGONA(samsung) <laff3y2297@gmail.com>
2024-09-09 13:04:17 +0900

    실습 8 - 2

--- 실습 2/실습 2/array_sum.cpp
+++ 실습 2/실습 2/array_sum.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <thread>
 #include <mutex>
@@ -6,56 +5,38 @@
 #include <vector>
 #include <atomic>
 
-volatile int sum;
+const int MAX_THREADS = 16;
+volatile int sum[MAX_THREADS];
 std::mutex my_lock;
 
-void worker_nolock(const int num_loop)
+void worker_nolock(const int th_id, const int num_loop)
 {
 	for (auto i = 0; i < num_loop; ++i) {
-		sum += 2;
-	}
-}
-
-void worker_opt(const int num_loop)
-{
-	volatile int local_sum = 0;
-	for (auto i = 0; i < num_loop; ++i) {
-		local_sum = local_sum + 2;
-	}
-	my_lock.lock();
-	sum = sum + local_sum;
-	my_lock.unlock();
-}
-
-void worker(const int num_loop)
-{
-	for (auto i = 0; i < num_loop; ++i) {
-		my_lock.lock();
-		sum = sum + 2;
-		my_lock.unlock();
+		sum[th_id] = sum[th_id] + 2;
 	}
 }
 
 int main()
 {
 	using namespace std::chrono;
-	// 너무 오래걸려서 0 한개 빴음
 	for (int n = 1; n < 16; ++n) {
-		sum = 0;
+		for (auto& n : sum) n = 0;
 		std::vector<std::thread> tv;
 		auto start_t = high_resolution_clock::now();
 		for (int i = 0; i < n; ++i) {
 			int num_loop = 50000000 / n;
 			if (i == (n - 1))
 				num_loop += 50000000 % num_loop;
-			tv.emplace_back(worker_opt, num_loop);
+			tv.emplace_back(worker_nolock, i, num_loop);
 		}
 		for (auto& th : tv)
 			th.join();
+		int result = 0;
+		for (auto n : sum) result = result + n;
 		auto end_t = high_resolution_clock::now();
 		auto exec_t = end_t - start_t;
 		size_t ms = duration_cast<milliseconds>(exec_t).count();
 		std::cout << "Duration: " << ms << "msecs" << std::endl;
-		std::cout << n << "Threads, Sum: " << sum << std::endl;
+		std::cout << n << "Threads, Sum: " << result << std::endl;
 	}
 }
