7828896bb9fffee12869e453a9c79dff88500afa
D4LGONA(samsung) <laff3y2297@gmail.com>
2024-09-25 11:50:34 +0900

    16-2

--- 2장/16.cpp
+++ 2장/16.cpp
@@ -27,8 +27,18 @@ void worker_1()
 int main()
 {
 	//bounce = new int{ 0 };
-	// 주소가 깔끔하게 들어가지 ㅇ낳았을 겨우 문제가 생김
-	int array[128]
+	// 주소가 깔끔하게 들어가지 않았을 경우 문제가 생김
+	int arr[64];
+	int* p = &(arr[32]);
+
+	long long addr = reinterpret_cast<long long>(p);
+	addr = (addr / 64) * 64; 
+	addr = addr - 2; // 얘를 추가했을떄 에러가 많이 생김.
+	// 왜? 
+
+	bounce = reinterpret_cast<int*>(addr);
+	*bounce = 0;
+
 	std::thread r{ worker_0 };
 	std::thread s{ worker_1 };
 
